53f7aac4fb53060244274d642e96ccbfa8ef0450
Henry Wallace <attobop@gmail.com>
2026-01-14 10:37:05 -0500

    Add .gitignore, remove target/

--- target/debug/deps/fynch-b5a52ed05d48c6d1.d
+++
@@ -1,6 +0,0 @@
-/Users/arc/Documents/dev/fynch/target/debug/deps/fynch-b5a52ed05d48c6d1.d: src/lib.rs src/metrics.rs
-
-/Users/arc/Documents/dev/fynch/target/debug/deps/libfynch-b5a52ed05d48c6d1.rmeta: src/lib.rs src/metrics.rs
-
-src/lib.rs:
-src/metrics.rs:
--- target/debug/deps/fynch-e2ee344f88e15cbd.d
+++
@@ -1,6 +0,0 @@
-/Users/arc/Documents/dev/fynch/target/debug/deps/fynch-e2ee344f88e15cbd.d: src/lib.rs src/metrics.rs
-
-/Users/arc/Documents/dev/fynch/target/debug/deps/fynch-e2ee344f88e15cbd: src/lib.rs src/metrics.rs
-
-src/lib.rs:
-src/metrics.rs:
